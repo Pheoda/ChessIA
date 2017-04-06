30777270bb17b99c150357781f7f58669173442e
Pheoda <n.fondacci@gmail.com>
2017-04-06 12:02:03 +0200

    Ajout classes necessaires

---
+++ ChessIA/ChessIA/Chessboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessIA
+{
+    class Chessboard
+    {
+        private Cell[,] board;
+
+        public Chessboard()
+        {
+            board = new Cell[8,8];
+        }
+
+
+    }
+}
---
+++ ChessIA/ChessIA/King.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessIA
+{
+    class King : Piece
+    {
+		public King(bool black) : base(black)
+        {
+
+        }
+    }
+}
