86f1dac2cbf71ad2b950650d8dd024bb5d104768
Panderium <alexis.fougerouse2@etu.univ-lyon1.fr>
2017-04-06 10:13:35 +0200

    Init project + add tlp

---
+++ ChessIA/ChessIA/ChessIA.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ChessIA
+{
+    partial class ChessIA
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chessboard = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // chessboard
+            // 
+            this.chessboard.ColumnCount = 8;
+            this.chessboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.Location = new System.Drawing.Point(377, 12);
+            this.chessboard.Name = "chessboard";
+            this.chessboard.RowCount = 8;
+            this.chessboard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.chessboard.Size = new System.Drawing.Size(650, 650);
+            this.chessboard.TabIndex = 0;
+            this.chessboard.CellPaint += new System.Windows.Forms.TableLayoutCellPaintEventHandler(this.chessboard_CellPaint);
+            // 
+            // ChessIA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1343, 714);
+            this.Controls.Add(this.chessboard);
+            this.Name = "ChessIA";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ChessIA_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel chessboard;
+    }
+}
+
---
+++ ChessIA/ChessIA/ChessIA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChessIA
+{
+    public partial class ChessIA : Form
+    {
+        public ChessIA()
+        {
+            InitializeComponent();
+        }
+
+        private void ChessIA_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chessboard_CellPaint(object sender, TableLayoutCellPaintEventArgs e)
+        {
+            if ((e.Column % 2 == 0))
+            {
+
+            }
+        }
+    }
+}
